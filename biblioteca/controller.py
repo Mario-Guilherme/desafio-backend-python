42020724b28cbfaeeb4a50a94198e496b85da8b8
Mario-Guilherme <carvalho16car@gmail.com>
2021-11-26 02:59:33 +0800

    feat: create class BibliotecaResource

--- biblioteca/controller.py
+++ biblioteca/controller.py
@@ -1,1 +1,14 @@
-from flask_restx import Resource, Api
+from flask_restx import Namespace, Resource
+
+from models import Biblioteca
+from schema import BibliotecaSchema
+from service import BibliotecaService
+from interface import BibliotecaInterface
+
+
+api = Namespace("Biblioteca", description="Back-end Sistema de biblioteca")
+
+
+@api.route("/obras")
+class BibliotecaResource(Resource):
+    pass
