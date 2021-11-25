b5f002b745797fa78d2b1550177c4c8d627a3ff8
Mario-Guilherme <carvalho16car@gmail.com>
2021-11-25 15:21:38 +0800

    Revert "feat: create fuction update"
    This reverts commit b19cc74d3b911497dd0ce1975aae033c831092e1.

--- biblioteca/interface.py
+++ biblioteca/interface.py
@@ -1,11 +1,5 @@
 from typing import Dict
-from typing import List
-from typing import Union
 
 
-class BibliotecaInterface(Dict[Union[int, str, List[str]]]):
-    id: int
-    titulo: str
-    editora: str
-    foto: str
-    autores: List[str]
+class BibliotecaInterface:
+    pass
