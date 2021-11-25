bfe9d3f6845b76155dde6bc181749b1ec1e9ee20
Mario-Guilherme <carvalho16car@gmail.com>
2021-11-25 15:04:35 +0800

    feat: create fuction delete_by_id

--- biblioteca/service.py
+++ biblioteca/service.py
@@ -5,8 +5,20 @@ from database import db_session
 
 from typing import List
 
+from interface import BibliotecaInterface
+
 
 class BibliotecaService:
     @staticmethod
     def get_all() -> List[Biblioteca]:
         return Biblioteca.query.all()
+
+    @staticmethod
+    def delete_by_id(id: int) -> List[int]:
+        biblioteca = Biblioteca.query.filter(Biblioteca.id == id).first()
+        if not biblioteca:
+            return []
+        db_session.delete(biblioteca)
+        db_session.commit()
+
+        return [id]
