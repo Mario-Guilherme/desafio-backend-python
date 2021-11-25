b12a9a71d39dd5db96923ff7bd93ed6adbd9aa2e
Mario-Guilherme <carvalho16car@gmail.com>
2021-11-26 03:21:42 +0800

    feat: add fuction get int BibliotecaResource

--- biblioteca/controller.py
+++ biblioteca/controller.py
@@ -1,14 +1,25 @@
+from flask import Flask
 from flask_restx import Namespace, Resource
+from flask_accepts import accepts, responds
+from flask.wrappers import Response
 
 from models import Biblioteca
 from schema import BibliotecaSchema
 from service import BibliotecaService
 from interface import BibliotecaInterface
 
+app = Flask(__name__)
 
 api = Namespace("Biblioteca", description="Back-end Sistema de biblioteca")
 
 
-@api.route("/obras")
+@api.route("/obras/")
 class BibliotecaResource(Resource):
-    pass
+    @accepts(schema=BibliotecaSchema, api=api)
+    @responds(schema=BibliotecaSchema)
+    def get(self) -> Response:
+        return Response(response=BibliotecaService.get_all(), status=200)
+
+
+if __name__ == "main":
+    app.run(port=5000, debug=False)
