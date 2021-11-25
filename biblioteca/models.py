444834fe46c2422f03e7af5639c904115c2693ea
Mario-Guilherme <carvalho16car@gmail.com>
2021-11-25 10:29:35 +0800

    feat:create class Biblioteca

---
+++ biblioteca/models.py
@@ -0,0 +1,32 @@
+from sqlalchemy import Column, Integer, String, ARRAY
+
+from database import Base
+
+from typing import List
+
+
+class Biblioteca(Base):
+    __tablename__ = "biblioteca"
+
+    id: int = Column(Integer, primary_key=True)
+    titulo: str = Column(String)
+    editora: str = Column(String)
+    foto: str = Column(String)
+    autores: str = Column(ARRAY(String))
+
+    def __init__(
+        self,
+        id: int = None,
+        titulo: str = None,
+        editora: str = None,
+        foto: str = None,
+        autores: List[str] = None,
+    ) -> None:
+
+        self.id = id
+        self.titulo = titulo
+        self.editora = editora
+        self.autores = autores
+
+    def __repr__(self) -> str:
+        return f"<Title {self.titulo}>"
