b19cc74d3b911497dd0ce1975aae033c831092e1
Mario-Guilherme <carvalho16car@gmail.com>
2021-11-25 15:19:37 +0800

    feat: create fuction update

--- biblioteca/interface.py
+++ biblioteca/interface.py
@@ -1,5 +1,11 @@
 from typing import Dict
+from typing import List
+from typing import Union
 
 
-class BibliotecaInterface:
-    pass
+class BibliotecaInterface(Dict[Union[int, str, List[str]]]):
+    id: int
+    titulo: str
+    editora: str
+    foto: str
+    autores: List[str]
