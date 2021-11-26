f7ed69242254fb48e6d980959dd44c0708ca6391
Mario-Guilherme <carvalho16car@gmail.com>
2021-11-26 10:51:22 +0800

    chore(pep8): add F811 in ignore

--- biblioteca/schema.py
+++ biblioteca/schema.py
@@ -1,15 +1,25 @@
+from flask_restx import model
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema, auto_field
 from marshmallow import fields
 
-from biblioteca.models import Livro
+from biblioteca.models import Livro, Autor
 
 from database import db_session
 
 
 class LivroSchema(SQLAlchemyAutoSchema):
     class Meta:
-        model: Livro = Livro
+        model = Livro
         sql_session = db_session
         load_instance = True
+
+    id = auto_field()
+
+
+class AutorSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Autor
+        sql_session = db_session
+        load_instace = True
 
     id = auto_field()
