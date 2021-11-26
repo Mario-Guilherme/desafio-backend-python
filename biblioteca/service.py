46bb7e357270a3a7004deef9dc204f245a1e4d78
Mario-Guilherme <carvalho16car@gmail.com>
2021-11-27 01:05:54 +0800

    refactor: refactor fuction create and create fuction create_autor and create_livro

--- biblioteca/service.py
+++ biblioteca/service.py
@@ -1,4 +1,3 @@
-from sqlalchemy.orm import session
 from biblioteca.models import Livro, Autor
 from database import db_session
 from biblioteca.interface import LivroInterface, AutorInterface
@@ -6,11 +5,16 @@ from biblioteca.schema import LivroSchema, AutorSchema
 
 from typing import List, Union
 
+import json
+
 
 interface_livro_autor = Union[LivroInterface, AutorInterface]
 
 
 class LivroService:
+    def __init__(self) -> None:
+        pass
+
     @staticmethod
     def get_all() -> List[Livro]:
 
@@ -27,30 +31,30 @@ class LivroService:
         return [id]
 
     @staticmethod
-    def __save_db(value):
+    def __save_db(value) -> None:
         db_session.add(value)
         db_session.commit()
 
+    def create_livro(self, titulo: str, editora: str, foto: str) -> Livro:
+        livro = Livro(titulo=titulo, editora=editora, foto=foto)
+        self.__save_db(livro)
+        return livro
+
+    def create_autor(self, livro_id: int, autores: List[str]) -> List[Autor]:
+
+        autores_list = []
+        for autor_x in autores:
+            autor = Autor(autor=autor_x, livro_id=livro_id)
+            self.__save_db(autor)
+            autores_list.append(autor_x)
+        print(autores_list)
+        return autores_list
+
     def create(self, data_atributte: interface_livro_autor) -> interface_livro_autor:
 
-        livro = Livro(
-            titulo=data_atributte["titulo"],
-            editora=data_atributte["editora"],
-            foto=data_atributte["foto"],
+        livro = self.create_livro(
+            data_atributte["titulo"], data_atributte["editora"], data_atributte["foto"]
         )
-        self.__save_db(livro)
 
-        livro_schema = LivroSchema()
-        livro_schema.load()
-
-        return livro_schema.dump(livro)
-
-    def create_autor(self, livro_id, autores) -> List[Autor]:
-
-        autores = []
-        for autor_x in autores:
-            autor = Autor(autor_x, livro_id)
-            autores.append(autor_x)
-            self.__save_db(autor)
-
-        return autores
+        self.create_autor(livro.id, data_atributte["autores"])
+        return json.dumps(data_atributte)
