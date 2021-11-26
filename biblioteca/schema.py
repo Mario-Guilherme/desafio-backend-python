d8248accfb03fb8e15b4a67e9ad56551f6677263
Mario-Guilherme <carvalho16car@gmail.com>
2021-11-26 10:40:44 +0800

    style: rename class BibliotecaSchema to LivroSchema

--- biblioteca/schema.py
+++ biblioteca/schema.py
@@ -1,10 +1,15 @@
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
-from models import Biblioteca
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema, auto_field
+from marshmallow import fields
+
+from biblioteca.models import Livro
+
 from database import db_session
 
 
-class BibliotecaSchema(SQLAlchemyAutoSchema):
+class LivroSchema(SQLAlchemyAutoSchema):
     class Meta:
-        model: Biblioteca = Biblioteca
+        model: Livro = Livro
         sql_session = db_session
         load_instance = True
+
+    id = auto_field()
