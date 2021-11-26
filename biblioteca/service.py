14d80aca87dd30f94a51fc938fe092a642d5bc66
Mario-Guilherme <carvalho16car@gmail.com>
2021-11-26 13:07:42 +0800

    style rename BibliotecaService to LivroService

--- biblioteca/service.py
+++ biblioteca/service.py
@@ -1,21 +1,24 @@
-from models import Biblioteca
-
-
+from sqlalchemy.orm import session
+from biblioteca.models import Livro, Autor
 from database import db_session
+from biblioteca.interface import LivroInterface, AutorInterface
+from biblioteca.schema import LivroSchema, AutorSchema
 
-from typing import List
-
-from interface import BibliotecaInterface
+from typing import List, Union
 
 
-class BibliotecaService:
+interface_livro_autor = Union[LivroInterface, AutorInterface]
+
+
+class LivroService:
     @staticmethod
-    def get_all() -> List[Biblioteca]:
-        return Biblioteca.query.all()
+    def get_all() -> List[Livro]:
+
+        return Livro.query.all()
 
     @staticmethod
     def delete_by_id(id: int) -> List[int]:
-        biblioteca = Biblioteca.query.filter(Biblioteca.id == id).first()
+        biblioteca = Livro.query.filter(Livro.id == id).first()
         if not biblioteca:
             return []
         db_session.delete(biblioteca)
@@ -24,15 +27,26 @@ class BibliotecaService:
         return [id]
 
     @staticmethod
-    def create(data_atributte: BibliotecaInterface) -> Biblioteca:
-        biblioteca = Biblioteca(
+    def create(data_atributte: interface_livro_autor) -> interface_livro_autor:
+        print(data_atributte)
+
+        livro = Livro(
             titulo=data_atributte["titulo"],
             editora=data_atributte["editora"],
             foto=data_atributte["foto"],
-            autores=data_atributte["autores"],
         )
-
-        db_session.add(biblioteca)
+        db_session.add(livro)
         db_session.commit()
 
-        return biblioteca
+        autores = []
+        for autor_x in data_atributte["autores"]:
+            autor = Autor(autor_x, livro.id)
+            autores.append(autor_x)
+            db_session.add(autor)
+
+        livro_schema = LivroSchema()
+        livro_schema.load()
+        # novo_titulo = schema.load(data_atributte, session=db_session)
+        db_session.commit()
+
+        return livro_schema.dump(livro)
