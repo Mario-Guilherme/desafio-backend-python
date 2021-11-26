7aaaef388e3a99cc919ded61f22a6a203cbfe3ca
Mario-Guilherme <carvalho16car@gmail.com>
2021-11-27 03:56:44 +0800

    feat: add fuction delete

--- biblioteca/controller.py
+++ biblioteca/controller.py
@@ -1,25 +1,39 @@
-from flask import Flask
+from flask import request, abort, make_response
 from flask_restx import Namespace, Resource
 from flask_accepts import accepts, responds
 from flask.wrappers import Response
-
-from models import Biblioteca
-from schema import BibliotecaSchema
-from service import BibliotecaService
-from interface import BibliotecaInterface
-
-app = Flask(__name__)
-
-api = Namespace("Biblioteca", description="Back-end Sistema de biblioteca")
+from sqlalchemy.sql.expression import delete
 
 
-@api.route("/obras/")
+from biblioteca.models import Livro, Autor
+from biblioteca.schema import LivroSchema, AutorSchema
+from biblioteca.service import LivroService
+
+
+ns = Namespace("Biblioteca", description="Back-end Sistema de biblioteca")
+
+
+@ns.route("/obras/")
 class BibliotecaResource(Resource):
-    @accepts(schema=BibliotecaSchema, api=api)
-    @responds(schema=BibliotecaSchema)
     def get(self) -> Response:
-        return Response(response=BibliotecaService.get_all(), status=200)
+        livro_service = LivroService()
+        return Response(response=livro_service.get_all(), status=200)
+
+    # @accepts(schema=BibliotecaSchema,api=ns)
+    def post(self) -> Response:
+        data = request.json
+        livro_service = LivroService()
+        return Response(response=livro_service.create(data_atributte=data), status=201)
 
 
-if __name__ == "main":
-    app.run(port=5000, debug=False)
+@ns.route("/obras/<int:id>")
+class BibliotecaIdResource(Resource):
+    def delete(self, id):
+        livro = LivroService().find_id_livro(id)
+
+        if livro is not None:
+            autores = LivroService().find_id_autor(livro.id)
+            LivroService().delete_by_id(livro, autores)
+            return make_response(f"Livro {id} deleted", 200)
+        else:
+            abort(404, f"Livro not found for Id: {id}")
