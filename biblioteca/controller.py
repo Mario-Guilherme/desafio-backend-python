e3d06689324ed8f39c5cc69cf1a9fe207746cf0f
Mario-Guilherme <carvalho16car@gmail.com>
2021-11-27 09:46:06 +0800

    refactor:  remove elif

--- biblioteca/controller.py
+++ biblioteca/controller.py
@@ -1,3 +1,4 @@
+import json
 from flask import request, abort, make_response
 from flask_restx import Namespace, Resource
 from flask_accepts import accepts, responds
@@ -28,19 +29,13 @@ class BibliotecaResource(Resource):
 
 @ns.route("/obras/<int:id>/")
 class BibliotecaIdResource(Resource):
-    def put(self, id: int):
+    def put(self, id: int) -> Response:
         livro_service = LivroService()
         livro = livro_service.find_id_livro(id)
         data = request.json
-        print(data)
 
-        existing_livro = livro_service.find_existing_livro(
-            data["titulo"], data["editora"], data["foto"]
-        )
         if livro is None:
             abort(404, f"Livro not found for Id: {id}")
-        elif existing_livro is not None and existing_livro.id != id:
-            abort(409, "Livro exists already")
         else:
             autores = livro_service.find_id_autor(livro.id)
             livro_service.update_livro(
@@ -48,7 +43,7 @@ class BibliotecaIdResource(Resource):
             )
             livro_service.update_autor(autores, data["autores"], livro.id)
 
-        return data
+        return Response(response=json(data), status=200)
 
     def delete(self, id):
         livro = LivroService().find_id_livro(id)
@@ -56,6 +51,6 @@ class BibliotecaIdResource(Resource):
         if livro is not None:
             autores = LivroService().find_id_autor(livro.id)
             LivroService().delete_by_id(livro, autores)
-            return make_response(f"Livro {id} deleted", 200)
+            return Response(response=f"Livro {id} deleted", status=200)
         else:
             abort(404, f"Livro not found for Id: {id}")
