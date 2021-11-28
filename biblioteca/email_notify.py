9fda0ef8109a224fb9ad42d8eae027c9f035493f
Mario-Guilherme <carvalho16car@gmail.com>
2021-11-28 10:08:04 +0800

    feat: create class to send email

---
+++ biblioteca/email_notify.py
@@ -0,0 +1,15 @@
+import smtplib
+import ssl
+
+
+class SendEmail:
+    def __init__(self, port=465, receiver_email=None) -> None:
+
+        self.__sender_email = "auxilioestudantilnotificador@gmail.com"
+        self.__password = "Shr@46/*+as/ASasj_*$%1"
+        self.__server_email = "smtp.gmail.com"
+        self.__receiver_email = receiver_email
+        self.__context = ssl.create_default_context()
+        self.__server = smtplib.SMTP_SSL(
+            self.__server_email, port, context=self.__context
+        )
