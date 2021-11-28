92a199ef9af4b8eb772cd2803ffc8250d36b4afd
Mario-Guilherme <carvalho16car@gmail.com>
2021-11-28 10:09:19 +0800

    feat: create fuction to login

--- biblioteca/email_notify.py
+++ biblioteca/email_notify.py
@@ -13,3 +13,6 @@ class SendEmail:
         self.__server = smtplib.SMTP_SSL(
             self.__server_email, port, context=self.__context
         )
+
+    def __login(self):
+        return self.__server.login(self.__sender_email, self.__password)
